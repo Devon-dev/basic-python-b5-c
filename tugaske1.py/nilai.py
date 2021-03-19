7f8b39c13a7f9d5728c85f95fa95c4a380a1e726
Devon Tanudjaja <devontanudjaja46@gmail.com>
2021-03-19 14:23:16 +0700

    Pesan

---
+++ tugaske1.py/nilai.py
@@ -0,0 +1,22 @@
+print("Belajar Kondisi IF-ELSE di Pemrogaman Python")
+
+kkm_mtk = 70
+nilai_fahmi = 75
+nilai_windayani = 95
+nilai_ayu = 60
+
+if nilai_fahmi >= kkm_mtk:
+   print("Lulus")
+else :
+   print("Tidak Lulus")
+
+
+if nilai_wandayani >= kkm_mtk:
+   print("Lulus")
+else : 
+   print("Tidak Lulus")
+
+if nilai_ayu >= kkm_mtk:
+   print("Lulus")
+else:
+   print("Tidak Lulus")
